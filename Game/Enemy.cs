9c57f2dc5ede7b388bd87c5126e9aa21f7f8579f
Tal Leibman <leibman2@gmail.com>
2019-02-19 13:14:07 +0200

    all the game logic is now in a class library added settings class , each class in "Game" namespace use a settings instance as its parameters for the ctor new class Engine mange the game the Enemies collection is now a queue ,  allowing for adding and removing Enemy during the game , and avoiding unnecessary collision checks

---
+++ Game/Enemy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Shapes;
+
+namespace Game
+{
+    public class Enemy : Entity
+    {
+        public Enemy(Settings settings)
+        {
+            Speed = settings.EnemySpeed;
+            Radius = settings.EnemyRadius;
+            Circle = new Ellipse();
+            Circle.Stroke = new SolidColorBrush(Colors.Red);
+            Circle.StrokeThickness = 2;
+            Circle.Height = Radius * 2;
+            Circle.Width = Radius * 2;
+        }
+
+        //chase player in a straight line with fixed speed
+        public void Move(double playerX, double playerY)
+        {
+
+            double deltaX = Math.Abs(X - playerX);
+            double deltaY = Math.Abs(Y - playerY);
+            double alpha = Math.Atan(deltaY / deltaX);
+
+            // move by X
+            if (playerX < X)
+            {
+                X -= Speed * Math.Cos(alpha);
+            }
+            else if (playerX > X)
+            {
+                X += Speed * Math.Cos(alpha);
+            }
+
+            // move by Y
+            if (playerY < Y)
+            {
+                Y -= Speed * Math.Sin(alpha);
+            }
+            else if (playerY > Y)
+            {
+                Y += Speed * Math.Sin(alpha);
+            }
+
+        }
+
+    }
+}
