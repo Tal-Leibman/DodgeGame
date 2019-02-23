a791f51721bb7d36bf6ec9db933deff34aafc4fd
Tal Leibman <leibman2@gmail.com>
2019-02-23 21:36:24 +0200

    struct to handle the keyboard input

---
+++ Game/PlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public struct PlayerInput
+    {
+        public bool Up { get; set; }
+        public bool Down { get; set; }
+        public bool Left { get; set; }
+        public bool Right { get; set; }
+    }
+}
